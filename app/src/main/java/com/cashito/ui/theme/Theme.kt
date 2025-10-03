3de553084b3e09974a41b80a4468e5f59979b4dc
FerZacVal <ferzac21@gmail.com>
2025-10-03 18:05:38 -0500

    theme

--- app/src/main/java/com/cashito/ui/theme/Theme.kt
+++ app/src/main/java/com/cashito/ui/theme/Theme.kt
@@ -1,4 +1,5 @@
 package com.cashito.ui.theme
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -6,10 +7,19 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+
+@Immutable
+data class ExtendedColorScheme(
+    val onPrimary: ColorFamily,
+    val onSecondary: ColorFamily,
+    val background: ColorFamily,
+)
 
 private val lightScheme = lightColorScheme(
     primary = primaryLight,
