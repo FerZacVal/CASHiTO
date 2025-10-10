b86fbc11729152f24704dbbe83e988d70c7fd848
FerZacVal <ferzac21@gmail.com>
2025-10-09 20:02:54 -0500

    modulos

--- app/src/main/java/com/cashito/CashitoApplication.kt
+++ app/src/main/java/com/cashito/CashitoApplication.kt
@@ -2,17 +2,19 @@ package com.cashito
 
 import android.app.Application
 import com.cashito.di.dataModule
+import com.cashito.di.domainModule
+import com.cashito.di.uiModule
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 class CashitoApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-
         startKoin {
+            androidLogger()
             androidContext(this@CashitoApplication)
-            // Cargas todos los módulos de Koin que definen cómo construir tus dependencias.
-            modules(dataModule)
-    }
+            modules(dataModule, domainModule, uiModule)
+        }
     }
 }
